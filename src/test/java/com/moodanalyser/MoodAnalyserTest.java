a9bf1a689f6548135e056709a55a6726e6f1668e
kiranudhan <udhankiran62@gmail.com>
2023-01-12 12:13:40 +0530

    [KiranUdhan]->MoodAnalyzer file is added

---
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,21 @@
+package com.moodanalyser;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+    @Test
+    public void given_messageWhenSadShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in Sad Mood");
+        Assert.assertEquals("Sad",mood);
+    }
+
+    @Test
+    public void given_messageWhenHappyShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in Happy Mood");
+        Assert.assertEquals("Happy",mood);
+    }
+}
